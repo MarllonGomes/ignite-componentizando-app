65d3e627345fc55bbe20a8e999e8e2f0d811f512
Marllon Gomes <marllon.fgomes@gmail.com>
2021-11-01 22:04:08 -0300

    exercício finalizado

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,33 @@
-export function SideBar() {
-  // Complete aqui
+import {Button} from "./Button";
+
+interface SidebarProps {
+    genres: Array<{
+        id: number;
+        name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+        title: string;
+    }>,
+    selectedGenreId: number,
+    onClickGenreCB(id: number): void
+}
+
+export function SideBar({genres, onClickGenreCB, selectedGenreId}: SidebarProps) {
+
+    return (
+        <nav className="sidebar">
+            <span>Watch<p>Me</p></span>
+
+            <div className="buttons-container">
+                {genres.map(genre => (
+                    <Button
+                        key={String(genre.id)}
+                        title={genre.title}
+                        iconName={genre.name}
+                        onClick={() => onClickGenreCB(genre.id)}
+                        selected={selectedGenreId === genre.id}
+                    />
+                ))}
+            </div>
+
+        </nav>
+    )
 }
